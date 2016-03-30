62f4a58d3c6be1ee083ff1eb0cfb147135741aa3
Derek Ekins <derek@spathi.com>
2016-03-30 21:24:55 +0100

    enabled integration test with plastiq (it works)

--- test/plastiqIntegrationSpec.js
+++ test/plastiqIntegrationSpec.js
@@ -2,26 +2,34 @@ var monkey = require('..');
 var plastiq = require('plastiq');
 var h = plastiq.html;
 
-xdescribe('plastiq integration', function(){
+describe('plastiq integration', function(){
   it('should find things rendered by plastiq', function(){
     function render(model){
       function renderMessage(){
         if(model.show) {
-          return h('div', 'hello');
+          return h('span', 'hello');
         }
       }
       return h('div', [
-          h('a.toggle'),
+          h('a.toggle', {
+            onclick: function(){
+              model.show = !model.show;
+            }
+          }),
           renderMessage()
         ]
       );
     }
-    var browser = monkey.create(render({}));
+
+    var vdom = h('div');
+
+    plastiq.appendVDom(vdom, render, {}, { requestRender: setTimeout, window: {} });
+    var browser = monkey.create(vdom);
     var vquery = require('vdom-query')
     browser.set({$: vquery, visibleOnly: false});
 
     return browser.find('.toggle').click().then(function(){
-      return browser.find('div', {text: 'hello'}).shouldExist;
+      return browser.find('span', {text: 'hello'}).shouldExist();
     })
   });
 });
