8f08906accc2b6a021be65129e83a8f4c5858168
Derek Ekins <derek@spathi.com>
2016-04-02 15:53:03 +0100

    add jquery/vdomquery to test method

--- test/scopeSpec.js
+++ test/scopeSpec.js
@@ -1,7 +1,7 @@
 var domTest = require('./domTest');
 
 describe('scope', function () {
-  domTest('can scope with an element', function (browser, dom) {
+  domTest('can scope with an element', function (browser, dom, $) {
     var red = dom.insert('<div><div class="element">red</div></div>');
     var blue = dom.insert('<div><div class="element">blue</div></div>');
 
@@ -14,7 +14,7 @@ describe('scope', function () {
     });
   });
 
-  domTest('can scope with another finder', function (browser, dom) {
+  domTest('can scope with another finder', function (browser, dom, $) {
     var red = dom.insert('<div class="red"><div class="element">red</div></div>');
     var blue = dom.insert('<div class="blue"><div class="element">blue</div></div>');
 
