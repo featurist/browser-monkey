c80015313970da9e7fb4309cc524c165d7c62709
Derek Ekins <derek@spathi.com>
2016-12-05 09:54:37 +0000

    enabled browser stack tests for IE 11

--- karma.conf.js
+++ karma.conf.js
@@ -112,7 +112,14 @@ var browsers = {
     os_version : 'El Capitan',
     resolution : '1024x768'
   },
-  'browserstack-edge': {
+  'browserstack-ie11': {
+    base: 'BrowserStack',
+    browser : 'IE',
+    browser_version : '11.0',
+    os : 'Windows',
+    os_version : '10',
+    resolution : '1024x768'
+  },'browserstack-edge': {
     base: 'BrowserStack',
     browser : 'Edge',
     browser_version : '13.0',
