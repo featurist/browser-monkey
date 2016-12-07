07a71f20f81c974590962bb93e96167b6582f4d6
Derek Ekins <derek@spathi.com>
2016-12-07 22:30:04 +0000

    enabled ie9 tests

--- karma.conf.js
+++ karma.conf.js
@@ -112,6 +112,14 @@ var browsers = {
     os_version : 'El Capitan',
     resolution : '1024x768'
   },
+  'browserstack-ie9': {
+    base: 'BrowserStack',
+    browser : 'IE',
+    browser_version : '9.0',
+    os : 'Windows',
+    os_version : '7',
+    resolution : '1024x768'
+  },
   'browserstack-ie10': {
     base: 'BrowserStack',
     browser : 'IE',
