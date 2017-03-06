f8878a579741857e4a6de1ab3d653a620756444d
Derek Ekins <derek@spathi.com>
2017-03-06 19:54:11 +0000

    use length instead of size() as size is deprecated

--- expectOneElement.js
+++ expectOneElement.js
@@ -4,5 +4,5 @@ var elementsToString = require('./elementsToString');
 
 module.exports = function expectOneElement(scope, elements) {
   var msg = "expected to find exactly one element: " + scope.printFinders(scope._finders) + ', but found :' + elementsToString(elements);
-  expect(elements.size(), msg).to.equal(1);
+  expect(elements.length, msg).to.equal(1);
 }
