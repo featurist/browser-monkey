c4cdb58e194b792875e4b4596f2edf4550a2e802
artemave <artemave@gmail.com>
2019-06-17 12:22:29 +0200

    remove unused code

--- test/tsSpec.js
+++
@@ -1,5 +0,0 @@
-import x from '../../browser-monkey/testA.ts'
-
-it('x', async () => {
-  console.log('x', await x())
-})
--- testA.ts
+++
@@ -1,7 +0,0 @@
-async function a () {
-  const x: number = 0
-  await new Promise(r => setTimeout(r, 100))
-  return x
-}
-
-export default a
