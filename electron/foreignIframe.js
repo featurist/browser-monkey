9c17599589fb829fd497da5a68878909ee8641cf
Tim Macfarlane <timmacfarlane@gmail.com>
2020-01-23 14:19:36 +0100

    added missing file

---
+++ electron/foreignIframe.js
@@ -0,0 +1,25 @@
+const { session, app, BrowserWindow } = require('electron')
+
+const omitHeaders = new Set([
+  'x-frame-options',
+  'content-security-policy'
+])
+
+app.on('ready', function () {
+  app.commandLine.appendSwitch('disable-pushstate-throttle')
+
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    Object.keys(details.responseHeaders).forEach(header => {
+      if (omitHeaders.has(header.toLowerCase())) {
+        delete details.responseHeaders[header]
+      }
+    })
+    callback({
+      cancel: false,
+      responseHeaders: details.responseHeaders,
+      statusLine: details.statusLine
+    })
+  })
+
+  BrowserWindow.addDevToolsExtension('react')
+})
